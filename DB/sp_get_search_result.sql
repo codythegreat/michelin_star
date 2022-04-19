cfc146c18841da2c1426af8f24f89516b9bd9c05
codythegreat <maxiecody@gmail.com>
2022-04-19 07:20:05 -0500

    adds id field

--- DB/sp_get_search_result.sql
+++ DB/sp_get_search_result.sql
@@ -1,7 +1,9 @@
 CREATE DEFINER=`root`@`localhost` PROCEDURE `michelin`.`sp_get_search_result`(IN _query varchar(512))
 BEGIN
 	SELECT
+		ms.id,
         ms.name,
+        d.description,
         ms.address,
         ms.location,
         ms.minprice,
@@ -14,12 +16,15 @@ BEGIN
         ms.url,
         ms.websiteurl,
         ms.award
-    FROM 
+    FROM
         michelin_star ms
+        left join descriptions d
+        	on ms.id = d.restaurant_id
     WHERE
     	ms.name LIKE CONCAT('%', _query , '%')
         or
         ms.address LIKE CONCAT('%', _query , '%')
         or
         ms.cuisine LIKE CONCAT('%', _query , '%');
+
 END
