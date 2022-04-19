4302eef4427f0f34eb96a76aae43f43badfcee58
codythegreat <maxiecody@gmail.com>
2022-04-19 07:22:47 -0500

    continued improvement of site

--- public/js/search.js
+++ public/js/search.js
@@ -1,9 +1,11 @@
 document.querySelectorAll('.restaurant-award').forEach(el => {
-    if (el.innerText = '3 MICHELIN Stars') {
-        el.innerText = '⭐⭐⭐'
-    } else if (el.innerText = '2 MICHELIN Stars') {
-        el.innerText = '⭐⭐'
-    } else if (el.innerText = '1 MICHELIN Star') {
-        el.innerText = '⭐'
+    if (el.innerText == '3 MICHELIN Stars') {
+        el.innerText = '⭐⭐⭐';
+    } else if (el.innerText == '2 MICHELIN Stars') {
+        el.innerText = '⭐⭐';
+    } else if (el.innerText == '1 MICHELIN Star') {
+        el.innerText = '⭐';
+    } else if (el.innerText == 'Bib Gourmand') {
+        el.innerText = '😋';
     }
 });
