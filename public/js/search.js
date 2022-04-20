5f4b11138b79416c14c94becb9147dfc688aecbd
codythegreat <maxiecody@gmail.com>
2022-04-20 10:30:16 -0500

    adds toggle for longer descriptions

--- public/js/search.js
+++ public/js/search.js
@@ -9,3 +9,31 @@ document.querySelectorAll('.restaurant-award').forEach(el => {
         el.innerText = '😋';
     }
 });
+
+document.querySelectorAll('.restaurant-description').forEach(el => {
+    if (el.innerText.length > 512) {
+        // split out text and add into span element
+        let text = el.innerText;
+        let short = el.innerText.substr(0, 512);
+        let long  = el.innerText.substr(512);
+        el.innerHTML = short + `<span class="dots" style="color: #333;">. . .</span><span class="long-text" style="display: none">${long}</span>`
+        // change cursor to show text is clickable
+        el.style.cursor = "pointer";
+        el.title = 'Expand text';
+        // add an event listener to show/hide span on click
+        el.addEventListener('click', function() {
+            // get the span
+            let dots = el.querySelector('span.dots');
+            let span = el.querySelector('span.long-text');
+            // toggle the span
+            if (span.style.display == '') {
+                dots.style.display = '';
+                span.style.display = 'none';
+            } else {
+                dots.style.display = 'none';
+                span.style.display = '';
+            }
+        });
+
+    }
+});
